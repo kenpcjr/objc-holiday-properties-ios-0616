29dbf6f51e8b8c45277808aef915d1363de96af7
kenpcjr <kennethcooke@me.com>
2016-06-08 21:41:11 -0400

    All tests pass

--- objc-holiday-properties/FISAppDelegate.m
+++ objc-holiday-properties/FISAppDelegate.m
@@ -10,40 +10,47 @@
 }
 
 - (NSArray *)holidaysInSeason:(NSString *)season {
-    
-    return nil;
+    NSMutableArray *holidays = [@[]mutableCopy];
+    for (NSString *holiday in self.database[season]) {
+        [holidays addObject:holiday];
+    }
+    return holidays;
 }
 
 - (NSArray *)suppliesInHoliday:(NSString *)holiday
                       inSeason:(NSString *)season {
-    
-    return nil;
+    NSMutableArray *supplies = [[NSMutableArray alloc]init];
+    for (NSString *supply in self.database[season][holiday]) {
+        [supplies addObject:supply];
+    }
+    return supplies;
 }
 
 - (BOOL)holiday:(NSString* )holiday
      isInSeason:(NSString *)season {
-    
-    return nil;
+    BOOL isHolidayInSeason = [[self.database[season] allKeys] containsObject:holiday];
+    return isHolidayInSeason;
 }
 
 - (BOOL)supply:(NSString *)supply
    isInHoliday:(NSString *)holiday
       inSeason:(NSString *)season {
-    
-    return nil;
+    BOOL containsSupply = [self.database[season][holiday]containsObject:supply];
+    return containsSupply;
 }
 
 - (void)addHoliday:(NSString *)holiday
           toSeason:(NSString *)season {
-    
-    // no return
+//    Not sure why this works if the property is immutable
+
+    [self.database[season] setObject:[@[]mutableCopy] forKey:holiday];
 }
 
 - (void)addSupply:(NSString *)supply
         toHoliday:(NSString *)holiday
          inSeason:(NSString *)season {
-    
-    // no return
+    [self.database[season][holiday] addObject:supply];
+    NSLog(@"%@",self.database);
 }
 
 @end
